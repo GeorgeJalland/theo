6fe2726a07a49c9050d42fc2d45ae0825800f62a
George Jalland <george.jalland@gmail.com>
2025-02-11 08:20:21 +0000

    Random class improved logic and readability

--- app/random_helper.py
+++ app/random_helper.py
@@ -1,6 +1,5 @@
 import random
 import bisect
-from itertools import accumulate
 
 class Random:
     # need to unit test this and add docstrings, also add print function or equivalent to see properties
@@ -10,11 +9,12 @@ class Random:
         self._max = max
         self._exclude = sorted(exclude)
         self._random_ranges = self._create_ranges()
-        self._weights = self._calculate_weights()
         self._cumulative_probabilities = self._calculate_cumulative_probablities()
 
     def get_random_number(self):
-        return self._get_random_number_from_random_range()
+        index = bisect.bisect(self._cumulative_probabilities, random.random())
+        select_range = self._random_ranges[index]
+        return random.randrange(*select_range)
 
     def _create_ranges(self) -> list[tuple[int, int]]:
         exclude = self._exclude
@@ -23,34 +23,25 @@ class Random:
         prev = 0
         for curr in exclude + [self._max + 1]:
             if curr - prev > 1:
-                random_ranges.append((prev+1, curr-1))
+                random_ranges.append((prev+1, curr))
             prev = curr
 
         return random_ranges
 
-    def _calculate_weights(self) -> list[int]:
-        weights = []
-        for start, end in self._random_ranges:
-            weights.append(end - start + 1)
-        return weights
-
     def _calculate_cumulative_probablities(self) -> list[float]:
-        probabilities = []
-        for weight in self._weights:
-            probabilities.append(weight / (self._max - len(self._exclude)))
-        return list(accumulate(probabilities))
-    
-    def _get_random_number_from_random_range(self) -> int:
-        return random.randint(*self._get_random_range())
-    
-    def _get_random_range(self) -> tuple[int, int]:
-        index = bisect.bisect(self._cumulative_probabilities, random.random())
-        return self._random_ranges[index]
+        cum_probabilities = []
+        total = self._max - len(self._exclude)
+        prev_probability = 0
+        for start, end in self._random_ranges:
+            weight = end - start
+            probability = (weight / total)
+            cum_probabilities.append(probability + prev_probability)
+            prev_probability = probability
+        return cum_probabilities
 
 if __name__ == "__main__":
-    randomGen = Random(max=6, exclude=[1, 2, 3])
+    randomGen = Random(max=10, exclude=[1, 6, 10])
     print(f"ranges: {randomGen._random_ranges}")
     print(f"cum: {randomGen._cumulative_probabilities}")
-    print(f"weights: {randomGen._weights}")
     for i in range(10):
         print(f"random number: {randomGen.get_random_number()}")
