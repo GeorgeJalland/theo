6ac93f3eb19541035c50904ba60a2e82eb632a03
George Jalland <george.jalland@gmail.com>
2025-02-13 20:44:37 +0000

    next quote id taken by incrementing quout_id cookie; asycio used to create background task for incrementing quoute counter

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import json
-from collections import deque
+import random
 import urllib.parse
 from fastapi import FastAPI, Depends, HTTPException, Response, Cookie
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import Session, select, func
+from sqlmodel import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
+import asyncio
 
 from db import get_session, init_db
 from models import Quote, Counter
-import random_helper
 
 app = FastAPI()
 
@@ -27,20 +27,15 @@ async def startup_event():
     await init_db()
 
 @app.get("/quote")
-async def get_quote(response: Response, liked_quotes: str = Cookie(None), prev_quote_ids: str = Cookie(None), session: AsyncSession = Depends(get_session)):
-    await increment_quotes_served(session)
+async def get_quote(response: Response, quote_id: int = Cookie(None), liked_quotes: str = Cookie(None), session: AsyncSession = Depends(get_session)):
+    asyncio.create_task(increment_quotes_served())
 
-    prev_quote_ids_list = read_cookie_list_value(prev_quote_ids)
-    prev_quote_ids_deque = deque(prev_quote_ids_list, maxlen=DEQUE_MAX_LENGTH)
+    if not quote_id:
+        quote_id = await get_random_quote_id(session)
+    quote = await get_next_quote(session, quote_id)
 
-    next_quote_id = await get_next_quote_id(session, prev_quote_ids_list)
-    prev_quote_ids_deque.append(next_quote_id)
-    response.set_cookie(key="prev_quote_ids", value=encode_and_jsonify_list(list(prev_quote_ids_deque)), httponly=True)
-
-    has_user_liked_quote = next_quote_id in read_cookie_list_value(liked_quotes)
-
-    quote = await get_quote_record(session, next_quote_id)
-
+    has_user_liked_quote = quote.id in read_cookie_list_value(liked_quotes)
+    response.set_cookie(key="quote_id", value=quote.id, httponly=True) #add max expiry here
     return {**quote.model_dump() , "has_user_liked_quote": has_user_liked_quote}
 
 @app.get("/quotes-served")
@@ -74,21 +69,18 @@ async def like_quote(response: Response, quote_id: int, liked_quotes: str= Cooki
     response.set_cookie(key="liked_quotes", value=encoded_quotes_list, httponly=True)
     return {"id": quote.id, "likes": quote.likes}
 
-@app.post("/clear-cookies")
-async def clear_cookies(response: Response):
-    response.delete_cookie
-
 async def get_quote_record(session: AsyncSession, quote_id: int) -> Quote:
     statement = select(Quote).where(Quote.id == quote_id)
     result = await session.execute(statement)
     return result.scalar()
 
-async def increment_quotes_served(session: AsyncSession) -> None:
-    result = await session.execute(select(Counter))
-    quotes_served = result.scalar()
-    quotes_served.served += 1
-    session.add(quotes_served)
-    await session.commit()
+async def increment_quotes_served() -> None:
+    async for session in get_session():
+        result = await session.execute(select(Counter))
+        quotes_served = result.scalar()
+        quotes_served.served += 1
+        session.add(quotes_served)
+        await session.commit()
 
 def read_cookie_list_value(cookie: str) -> list:
     if not cookie:
@@ -102,16 +94,17 @@ def decode_and_parse_cookie(cookie: str):
 def encode_and_jsonify_list(list_input: str):
     return urllib.parse.quote(json.dumps(list_input))
 
-async def get_next_quote_id(session: AsyncSession, prev_ids: list):
-    next_quote_id = await get_random_quote_id_excluding_prev_ids(session, prev_ids)
-    return next_quote_id
+async def get_next_quote(session: AsyncSession, current_id: int) -> Quote:
+    max_id = await get_max_quote_id(session)
+    next_id = (current_id % max_id) + 1
+    next_quote = await session.execute(select(Quote).where(Quote.id == next_id))
+    return next_quote.scalar()
 
-async def get_random_quote_id_excluding_prev_ids(session: AsyncSession, prev_ids) -> int:
-    max_id = await get_max_id(session)
-    random_generator = random_helper.Random(max=max_id, exclude=prev_ids)
-    return random_generator.get_random_number()
+async def get_random_quote_id(session: AsyncSession) -> int:
+    max_id = await get_max_quote_id(session)
+    return random.randint(1, max_id)
 
-async def get_max_id(session: AsyncSession) -> int:
+async def get_max_quote_id(session: AsyncSession) -> int:
     statement = select(func.max(Quote.id))
     result = await session.execute(statement)
     max_id = result.scalar()
