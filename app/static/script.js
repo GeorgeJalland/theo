a62470fb2303b398f67bbf375340daeca79f4771
George Jalland <george.jalland@gmail.com>
2025-03-06 22:33:57 +0000

    listeners added for modal like and share, load functions for menu items, refactored states into objects

--- app/static/script.js
+++ app/static/script.js
@@ -4,7 +4,17 @@ const qotdButton = document.getElementById("menu-qotd");
 const leaderboardButton = document.getElementById("menu-leaderboard");
 const mainContainer = document.getElementById("mainContainer");
 const leaderboardContainer = document.getElementById("leaderboardContainer");
-const menuItems = [{"element": mainContainer, "button": qotdButton}, {"element": leaderboardContainer, "button": leaderboardButton}];
+const mainLoadFunction = () => {
+    fetchQuotesServedCount();
+    fetchQuote(qotdState.quoteId).then(() => {
+        history.pushState(qotdState, "", `?quoteId=${qotdState.quoteId}`)
+    })
+}
+const leaderboardLoadFunction = () => {
+    fetchTopQuotes("likes", leaderboardState.top_quotes_page, quote_limit);
+}
+const menuItems = [{ element: mainContainer, button: qotdButton, loadFunction: mainLoadFunction },
+{ element: leaderboardContainer, button: leaderboardButton, loadFunction: leaderboardLoadFunction }];
 
 const quoteText = document.getElementById("quoteText");
 const quoteLikes = document.getElementById("quoteLikes");
@@ -20,6 +30,7 @@ const innerQuoteContainer = document.getElementById("innerQuoteContainer");
 const quoteReference = document.getElementById("quoteReference");
 const shareButton = document.getElementById("shareButton");
 const leaderboard = document.getElementById("leaderboard");
+const leaderboardBody = document.querySelector("#leaderboard tbody")
 const menu = document.getElementById("menu")
 
 const modal = document.getElementById("modal-layout");
@@ -27,28 +38,53 @@ const quoteTextModal = document.getElementById("quoteTextModal");
 const quoteReferenceModal = document.getElementById("quoteReferenceModal");
 const quoteLikesModal = document.getElementById("quoteLikesModal");
 const quoteSharesModal = document.getElementById("quoteSharesModal");
+const likeButtonModal = document.getElementById("likeButtonModal");
+const shareButtonModal = document.getElementById("shareButtonModal");
+const likeArrowModal = document.getElementById("arrow2Modal");
+const likeMeModal = document.getElementById("likeMeModal");
 
 const growAnimations = ['grow', 'grow2', 'grow3']
 
 const isLocal = window.location.hostname === "localhost"
 const apiBase = window.location.protocol + '//' + window.location.hostname
 const apiRoot = "/api"
-const apiPort =  isLocal ? "7000" : ""
+const apiPort = isLocal ? "7000" : ""
 
-function buildApiString(endpoint){
+function buildApiString(endpoint) {
     return apiBase + ':' + apiPort + apiRoot + endpoint
 }
 
 const quote_limit = 10
 const searchParams = new URLSearchParams(window.location.search)
-// states
-let quoteId = parseInt(searchParams.get("quoteId"));
-let quoteIsLiked = false
-let userHasClickedLike = false
-let userHasClickedTheo = false
-let quotesServed = 0
-let top_quotes_page = 1
-let selectedMenuItem = {"element": mainContainer, "button": qotdButton}
+let selectedMenuItem = { "element": mainContainer, "button": qotdButton }
+// states, maybe qotd and modal can be instances of same class
+qotdState = {
+    quoteId: parseInt(searchParams.get("quoteId")),
+    quoteIsLiked: false,
+    userHasClickedLike: false,
+    userHasClickedTheo: false,
+    quotesServed: 0
+}
+
+leaderboardState = {
+    top_quotes_page: 1
+}
+
+modalState = {
+    quoteId: 0,
+    quoteIsLiked: false,
+    userHasClickedLike: false,
+    userHasClickedTheo: false,
+}
+
+
+function handleClickMenuItem(event) {
+    if (event.target.classList.contains("menuItem")) {
+        let menuItem = menuItems.filter(item => item.button === event.target)[0]
+        menuItem.loadFunction()
+        displayMenuItem(menuItem.element, menuItem.button)
+    }
+}
 
 function displayMenuItem(element, menuButton) {
     unselectElement(selectedMenuItem.button)
@@ -59,25 +95,41 @@ function displayMenuItem(element, menuButton) {
     selectedMenuItem.button = menuButton
 }
 
-function handleClickLike(localQuoteId) {
-    if (!userHasClickedLike) {
-        makeOpaque(likeArrow)
-        makeOpaque(likeMe)
-        userHasClickedLike = true
+function handleClickQuoteCell(event) {
+        unhideElement(modal)
+        quoteTextModal.textContent = event.target.textContent
+        quoteReferenceModal.href = event.target.dataset.ref
+        quoteLikesModal.textContent = event.target.dataset.likes
+        quoteSharesModal.textContent = event.target.dataset.shares
+        modalState.quoteId = event.target.dataset.quoteId
+
+        setLikeButtonColourInitial(modalState, likeButtonModal, event.target.dataset.hasUserLikedQuote)
+}
+
+function handleClickLike(state, likeButtonElement, quoteLikesElement, likeArrowElement, likeMeElement) {
+    if (!state.userHasClickedLike) {
+        makeOpaque(likeArrowElement)
+        makeOpaque(likeMeElement)
+        state.userHasClickedLike = true
     }
-    setLikeProperties(likeButton, quoteLikes, growAnimations)
-    likeQuote(localQuoteId)
-    quoteIsLiked = !quoteIsLiked
+    setLikeProperties(likeButtonElement, quoteLikesElement, growAnimations, state.quoteIsLiked)
+    likeQuote(state.quoteId)
+    state.quoteIsLiked = !state.quoteIsLiked
+}
+
+function handleClickLikeModal(state, likeButtonElement, quoteLikesElement, likeArrowElement, likeMeElement) {
+    handleClickLike(state, likeButtonElement, quoteLikesElement, likeArrowElement, likeMeElement)
+    // set the table row dataset likes to new likes value, consider storing in leaderboard state as opposed to table?
 }
 
 async function handleClickTheo(localQuoteId) {
-    if (!userHasClickedTheo) {
+    if (!qotdState.userHasClickedTheo) {
         makeOpaque(theoArrow)
         makeOpaque(clickMe)
-        userHasClickedTheo = true
+        qotdState.userHasClickedTheo = true
     }
     await fetchQuote(localQuoteId)
-    history.pushState({quoteId}, "", `?quoteId=${quoteId}`)
+    history.pushState(qotdState, "", `?quoteId=${qotdState.quoteId}`)
 }
 
 async function handleClickShare(localQuoteId) {
@@ -90,15 +142,15 @@ async function handleClickShare(localQuoteId) {
         console.log(await navigator.share(shareData));
         console.log("Quote successfully shared");
         shareQuote(localQuoteId)
-        setValueAndAnimation(quoteShares, parseInt(quoteShares.textContent)+1, growAnimations)
+        setValueAndAnimation(quoteShares, parseInt(quoteShares.textContent) + 1, growAnimations)
     } catch (error) {
         console.error("Error sharing quote: ", error);
     }
 }
 
 async function fetchQuote(localQuoteId) {
-    quotesServed += 1;
-    setValueAndAnimation(quotesServedCount, quotesServed, growAnimations, setLocaleString=true)
+    qotdState.quotesServed += 1;
+    setValueAndAnimation(quotesServedCount, qotdState.quotesServed, growAnimations, setLocaleString = true)
     const api_uri = localQuoteId ? "/quote" + "?" + "id=" + localQuoteId : "/quote"
     try {
         const response = await fetch(buildApiString(api_uri), {
@@ -108,13 +160,13 @@ async function fetchQuote(localQuoteId) {
         if (!response.ok) throw new Error("Failed to fetch");
 
         const data = await response.json();
-        updateTextWithAnimation(innerQuoteContainer, quoteText, '"'+data.text+'"', 'bounce', 500)
+        updateTextWithAnimation(innerQuoteContainer, quoteText, '"' + data.text + '"', 'bounce', 500)
         quoteLikes.textContent = data.likes;
         quoteShares.textContent = data.shares;
         quoteReference.href = data.reference;
-        quoteId = data.id;
+        qotdState.quoteId = data.id;
 
-        setLikeButtonColourInitial(likeButton, data.has_user_liked_quote);
+        setLikeButtonColourInitial(qotdState, likeButton, data.has_user_liked_quote);
     } catch (error) {
         quoteText.textContent = "Error loading quote.";
         console.error("Error:", error);
@@ -128,7 +180,7 @@ async function fetchQuotesServedCount() {
         });
         if (!response.ok) throw new Error("Failed to fetch");
         const data = await response.json();
-        quotesServed = data.quotes_served
+        qotdState.quotesServed = data.quotes_served
         quotesServedCount.textContent = data.quotes_served.toLocaleString();
 
     } catch (error) {
@@ -138,14 +190,15 @@ async function fetchQuotesServedCount() {
 }
 
 async function fetchTopQuotes(orderBy, page, limit) {
-    endpoint_uri = "/quotes"+"?"+"order_by="+orderBy+"&"+"page="+page+"&"+"size="+limit
+    endpoint_uri = "/quotes" + "?" + "order_by=" + orderBy + "&" + "page=" + page + "&" + "size=" + limit
     try {
         const response = await fetch(buildApiString(endpoint_uri), {
             method: "GET"
         });
         if (!response.ok) throw new Error("Failed to fetch");
         const data = await response.json();
-        data.items.forEach(function(item) {
+        leaderboardBody.innerHTML = "";
+        data.items.forEach(function (item) {
             const tr = document.createElement('tr');
             const likesCell = document.createElement('td');
             const quoteCell = document.createElement('td');
@@ -156,12 +209,13 @@ async function fetchTopQuotes(orderBy, page, limit) {
             quoteCell.dataset.ref = item.reference
             quoteCell.dataset.shares = item.shares
             quoteCell.dataset.likes = item.likes
+            quoteCell.dataset.hasUserLikedQuote = item.has_user_liked_quote
             quoteCell.classList = "quoteCell"
 
             tr.appendChild(likesCell);
             tr.appendChild(quoteCell);
-            leaderboard.appendChild(tr);
-          });
+            leaderboardBody.appendChild(tr);
+        });
         // populate some state with top quotes???
         // display in tables
 
@@ -200,7 +254,7 @@ async function shareQuote(localQuoteId) {
 function setValueAndAnimation(element, value, animationOptions, setLocaleString = false) {
     animation = getAnimationTypeFromCount(value, animationOptions)
     if (setLocaleString) {
-        value =  value.toLocaleString()
+        value = value.toLocaleString()
     }
     if (!animation) {
         element.textContent = value
@@ -212,9 +266,9 @@ function setValueAndAnimation(element, value, animationOptions, setLocaleString 
 function getAnimationTypeFromCount(count, options) {
     // This checks to see if count is multiple of powers of 10 and returns corresponding animation options
     for (let i = 0; i < options.length; i++) {
-        if (count % 10**(i+1) != 0) return options[i-1];
+        if (count % 10 ** (i + 1) != 0) return options[i - 1];
     }
-    return options[options.length-1]
+    return options[options.length - 1]
 }
 
 function updateTextWithAnimation(containerElement, element, newText, animation, duration) {
@@ -245,8 +299,8 @@ function makeOpaque(element) {
     element.classList.add("makeOpaque")
 }
 
-function setLikeButtonColourInitial(likeButtonLocal, hasUserLikedQuote) {
-    quoteIsLiked = hasUserLikedQuote
+function setLikeButtonColourInitial(state, likeButtonLocal, hasUserLikedQuote) {
+    state.quoteIsLiked = hasUserLikedQuote
     if (hasUserLikedQuote) {
         likeButtonLocal.classList.add("quoteLiked");
     } else {
@@ -254,9 +308,18 @@ function setLikeButtonColourInitial(likeButtonLocal, hasUserLikedQuote) {
     }
 }
 
-function setLikeProperties(likeButtonElement, quoteLikesElement, animationOptions) {
+function setLikeButtonModalColourInitial(likeButtonLocal, hasUserLikedQuote) {
+    modalState.quoteModalIsLiked = hasUserLikedQuote
+    if (hasUserLikedQuote) {
+        likeButtonLocal.classList.add("quoteLiked");
+    } else {
+        likeButtonLocal.classList.remove("quoteLiked");
+    }
+}
+
+function setLikeProperties(likeButtonElement, quoteLikesElement, animationOptions, quoteLiked) {
     // This function sets like count and button colour based on if like was previously selected, sets animation on positive increment
-    if (quoteIsLiked) {
+    if (quoteLiked) {
         quoteLikesElement.textContent = parseInt(quoteLikesElement.textContent) - 1
         likeButtonElement.classList.remove("quoteLiked");
     } else {
@@ -265,38 +328,31 @@ function setLikeProperties(likeButtonElement, quoteLikesElement, animationOption
     }
 }
 
-fetchQuotesServedCount();
-fetchQuote(quoteId).then(() => {
-    history.pushState({quoteId}, "", `?quoteId=${quoteId}`)
-});
-fetchTopQuotes("likes", top_quotes_page, quote_limit);
+// Functions called on page load
+mainLoadFunction();
 
 // listeners
-likeButton.addEventListener("click", () => handleClickLike(quoteId));
-theoPictureButton.addEventListener("click", () => handleClickTheo(quoteId+1));
-shareButton.addEventListener("click", () => handleClickShare(quoteId));
-menu.addEventListener("click", event => {
-    if (event.target.classList.contains("menuItem")) {
-        let menuItem = menuItems.filter(item => item.button === event.target)[0]
-        displayMenuItem(menuItem.element, menuItem.button)
-    }
-})
+menu.addEventListener("click", event => handleClickMenuItem(event))
+
+likeButton.addEventListener("click", () => handleClickLike(qotdState, likeButton, quoteLikes, likeArrow, likeMe));
+theoPictureButton.addEventListener("click", () => handleClickTheo(qotdState.quoteId + 1));
+shareButton.addEventListener("click", () => handleClickShare(qotdState.quoteId));
 leaderboard.addEventListener("click", event => {
     if (event.target.classList.contains("quoteCell")) {
-        unhideElement(modal)
-        quoteTextModal.textContent = event.target.textContent
-        quoteReferenceModal.href = event.target.dataset.ref
-        quoteLikesModal.textContent = event.target.dataset.likes
-        quoteSharesModal.textContent = event.target.dataset.shares
+        handleClickQuoteCell(event);
     }
-});
+})
+likeButtonModal.addEventListener("click", () => handleClickLikeModal(modalState, likeButtonModal, quoteLikesModal, likeArrowModal, likeMeModal));
+shareButtonModal.addEventListener("click", () => handleClickShare(modalState.quoteId));
+
 window.addEventListener("click", (event) => {
     if (event.target === modal) {
         hideElement(modal);
     }
 });
 window.addEventListener('popstate', (event) => {
+    console.log("popping state: ", event.state)
     if (event.state && event.state.quoteId) {
         fetchQuote(event.state.quoteId)
     }
-  });
+});
