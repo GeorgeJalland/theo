d06c7851101167b7ae466f6eef6a3322a60d9355
George Jalland <george.jalland@gmail.com>
2026-05-03 21:23:09 +0100

    lib added

---
+++ app/frontend/src/lib/utils.js
@@ -0,0 +1,45 @@
+export function buildApiString(endpoint) {
+  return `${process.env.NEXT_PUBLIC_API_URL}/api${endpoint}`
+}
+
+export function getAnimationTypeFromCount(count, options) {
+    console.log("Getting animation type for count: " + count, " with options: " + options)
+    // This checks to see if count is multiple of powers of 10 and returns corresponding animation options
+    for (let i = 0; i < options.length; i++) {
+        if (count % 10 ** (i + 1) != 0) return options[i - 1];
+    }
+    return options[options.length - 1]
+}
+
+export function applyAnimation(element, animation, duration) {
+    element.classList.add(animation);
+    setTimeout(() => {
+        element.classList.remove(animation);
+    }, duration);
+}
+
+export function hideElement(element) {
+    element.classList.add("hide")
+}
+
+export function unhideElement(element) {
+    element.classList.remove("hide")
+}
+
+export function selectElement(element) {
+    element.classList.add("selected")
+}
+
+export function unselectElement(element) {
+    element.classList.remove("selected")
+}
+
+export function makeOpaque(element) {
+    element.classList.add("makeOpaque")
+}
+
+export function updateCanonicalLinkWithUrl() {
+    const url = window.location.href
+    let canonicalLink = document.getElementById('canonicalLink');
+    canonicalLink.setAttribute("href", url);
+  }
