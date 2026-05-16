71e3651ef943467f5f912a2cbd48c013d8554d15
George Jalland <george.jalland@gmail.com>
2026-05-16 12:10:28 +0100

    episode page using fuzzy block

--- frontend/src/app/episode/[id]/page.jsx
+++ frontend/src/app/episode/[id]/page.jsx
@@ -8,6 +8,7 @@ import QuoteBlock from "@/components/QuoteBlock"
 import QuoteListServer from "../../../components/QuoteListServer";
 import QuoteListSkeleton from "../../../components/QuoteListSkeleton"
 import Panel from "@/components/Panel";
+import FuzzyBlock from "@/components/FuzzyBlock";
 
 export default async function Episode({params}){
     const { id } = await params
@@ -39,18 +40,22 @@ export default async function Episode({params}){
                     src={thumbnails[0].url}
                     className="h-100 w-100 border-2 border-white/80 rounded-2xl"
                 />
-                <div className="text-left">
-                    <h1 className="text-4xl">{episode.title}</h1>
-                    <h2>{episode.publish_date.split("T")[0]} ({timeAgo(episode.publish_date)} ago)</h2>
-                    <div className="flex gap-4 items-center">
-                        <Link href={episode.spotify_url} target="_blank" rel="noopener noreferrer" title="open spotify episode">
-                            <Image priority height={64} width={64} src="/images/spotify_logo.png" alt="spotify logo" className="w-14 h-full"/>
-                        </Link>
-                        <Link href={episode.youtube_url} target="_blank" rel="noopener noreferrer" title="open youtube video">
-                            <Image priority height={50} width={64} src="/images/youtube_icon.webp" alt="youtube logo" className="w-16 h-full"/>
-                        </Link>
+                
+                <FuzzyBlock>
+                    <div className="text-left">
+                        <h1 className="text-4xl">{episode.title}</h1>
+                        <h2>{episode.publish_date.split("T")[0]} ({timeAgo(episode.publish_date)} ago)</h2>
+                        <div className="flex gap-4 items-center">
+                            <Link href={episode.spotify_url} target="_blank" rel="noopener noreferrer" title="open spotify episode">
+                                <Image priority height={64} width={64} src="/images/spotify_logo.png" alt="spotify logo" className="w-14 h-full"/>
+                            </Link>
+                            <Link href={episode.youtube_url} target="_blank" rel="noopener noreferrer" title="open youtube video">
+                                <Image priority height={50} width={64} src="/images/youtube_icon.webp" alt="youtube logo" className="w-16 h-full"/>
+                            </Link>
+                        </div>
                     </div>
-                </div>
+                </FuzzyBlock>
+
             </div>
             <Panel className="mb-4">
                 <span className="text-6xl">[Quotes]</span>
